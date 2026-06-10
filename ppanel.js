57bed02e99528bed78a1327432b612e7fe568bc1
NovaSystem-DG <novasystemdigital@gmail.com>
2026-06-09 21:31:01 -0500

    fotos de cosa

--- ppanel.js
+++ ppanel.js
@@ -344,7 +344,7 @@ function getDefaultProducts() {
 }
 
 // ═══════════════════════════════════════════
-// INIT
+
 // ═══════════════════════════════════════════
 async function init() {
   const apiKey = getApiKey();
